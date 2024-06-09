9393fb8cc30f9f8911e66ccf59c87133a6c7f414
Missangela99 <angelikahardyn99@gmail.com>
2024-06-09 16:18:13 +0200

    made loop that adds number of divs to the container, began styling the css.

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 let container = document.querySelector(".container");
+
 let square = document.createElement("div");
+square.setAttribute("id", "square")
+let input = document.createElement("input");
+
+for (let i=0; i<16; i++) {
+    container.appendChild(square)
+}
