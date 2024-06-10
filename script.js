97fa8482219dd0bb7045a699a8f795b8c6a5756a
Missangela99 <angelikahardyn99@gmail.com>
2024-06-10 14:59:32 +0200

    created the dynamic grid, layered off first few ui elements

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 let container = document.querySelector(".container");
+let input = document.querySelector(".input");
+let submit = document.querySelector(".submit")
 
-let square = document.createElement("div");
-square.setAttribute("id", "square")
-let input = document.createElement("input");
+let rows = 16;
+let columns = 16;
 
-for (let i=0; i<16; i++) {
-    container.appendChild(square)
+
+
+for (i = 0; i < rows; i++) {
+
+    let row = document.createElement("div");
+    row.setAttribute("id", "row");
+    container.appendChild(row)
+
+
+    for ( j = 0; j < columns; j++) {
+        let column = document.createElement("div");
+        column.setAttribute("id", "column");
+        row.appendChild(column);
+
+    }
+
 }
+
