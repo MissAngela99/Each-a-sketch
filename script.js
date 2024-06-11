6bf7e531169982f6b23daf9c9711d4b4909313ea
Missangela99 <angelikahardyn99@gmail.com>
2024-06-11 17:58:28 +0200

    Cursor now is properly drawing the squares when hovered over

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
+let body = document.querySelector("body")
 let container = document.querySelector(".container");
-let userInput = document.querySelector(".input").value;
-let userClick = document.querySelector(".gridButton");
-
+let input = document.querySelector("input").value;
+let bunton = document.querySelector("button");
 let row = document.createElement("div");
+
 row.setAttribute("id", "row");
 let column = document.createElement("div");
-column.setAttribute("id", "column");
-let columns = 16;
+column.setAttribute("class", "column");
 
+let columns = 16;
 
     for (i = 0; i < columns; i++) {
 
         let row = document.createElement("div");
-
         container.appendChild(row)
 
 
         for ( j = 0; j < columns; j++) {
             let column = document.createElement("div");
-            column.setAttribute("id", "column");
+            column.setAttribute("class", "column");
             row.appendChild(column);
 
         }
 
     }
 
-const singleSquare = document.querySelectorAll("#column")
-
-const squares = Array.from(singleSquare)
+const square = document.querySelectorAll(".column");
+square.forEach((column) => {
+    column.addEventListener("mouseenter", () =>{
+        column.style.backgroundColor = 'black'
+    })
+})
