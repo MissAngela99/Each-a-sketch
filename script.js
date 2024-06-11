6d5883e6d353a6614399d6c8394140b6ff6ca58d
Missangela99 <angelikahardyn99@gmail.com>
2024-06-11 18:16:11 +0200

    Typing in the value and clicking 'switch' now creates more squares.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let body = document.querySelector("body")
 let container = document.querySelector(".container");
-let input = document.querySelector("input").value;
-let bunton = document.querySelector("button");
+let input = document.querySelector("input");
+let button = document.querySelector("button");
 let row = document.createElement("div");
 
 row.setAttribute("id", "row");
@@ -24,6 +24,34 @@ let columns = 16;
         }
 
     }
+button.addEventListener("click", () => {
+
+    columns = input.value;
+    container.textContent = '';
+
+    for (i = 0; i < columns; i++) {
+
+        let row = document.createElement("div");
+        container.appendChild(row)
+
+
+        for ( j = 0; j < columns; j++) {
+            let column = document.createElement("div");
+            column.setAttribute("class", "column");
+            row.appendChild(column);
+
+        }
+
+    }
+
+    const square = document.querySelectorAll(".column");
+    square.forEach((column) => {
+        column.addEventListener("mouseenter", () =>{
+            column.style.backgroundColor = 'black'
+    })
+})
+});
+
 
 const square = document.querySelectorAll(".column");
 square.forEach((column) => {
