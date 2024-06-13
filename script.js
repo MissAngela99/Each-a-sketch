09384296fba0a3f11016b6737b204d719a9beae1
Missangela99 <angelikahardyn99@gmail.com>
2024-06-13 13:20:19 +0200

    resize now doesn't change the size of the container, just amount of pixels on container

--- script.js
+++ script.js
@@ -21,9 +21,19 @@ let columns = 16;
             column.setAttribute("class", "column");
             row.appendChild(column);
 
-        }
 
-    }
+            let singleSquare = 600 / columns;
+            let columnSize = document.querySelector('.column');
+            columnSize.style.width = `${singleSquare}px`;
+            columnSize.style.height = `${singleSquare}px`;
+
+        };
+
+    };
+
+
+
+
 button.addEventListener("click", () => {
     if (input.value <= 100) {
         columns = input.value;
@@ -51,12 +61,24 @@ button.addEventListener("click", () => {
             column.style.backgroundColor = 'black';
             column.style.opacity = Math.min(columnShadow + 0.1, 1);
             column.style.border = '';
+
+
+            });
+ 
         });
+
+        let singleSquare = 600 / columns;
+        let columnSize = document.querySelectorAll('.column')
+        columnSize.forEach((column) => {
+            column.style.width = `${singleSquare}px`;
+            column.style.height = `${singleSquare}px`;
+            
         });
     } else if (input.value > 100) {
         columns = 0
         alert("Too high!");
     };
+
 });
 
 const square = document.querySelectorAll(".column");
@@ -68,3 +90,9 @@ square.forEach((column) => {
             column.style.border = '';
         });
 });
+                let singleSquare = 600 / columns;
+                let columnSize = document.querySelectorAll('.column')
+                columnSize.forEach((column) => {
+                    column.style.width = `${singleSquare}px`;
+                    column.style.height = `${singleSquare}px`;
+                });
